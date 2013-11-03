d4344e3e8afb584b6f2561b6338e3567060c614b
Christopher Schmidt <crschmidt@crschmidt.net>
2013-11-03 01:13:01 -0400

    Olllld presentation materials.

---
+++ present/barcamp.php
@@ -0,0 +1,66 @@
+<?php	
+include "../template.php";
+?>
+<title>BarCamp Boston Geowanking Presentation</title>
+<body>
+<h2>People</h2>
+<dl>
+<dt><a href="http://iconocla.st">Schuyler Erle</a></dt>
+<dd>Co-author, Mapping Hacks, Google Maps Hacks</dd>
+<dt><a href="http://frot.org">Jo Walsh</a></dt><dd> Co-author, Mapping Hacks</dd>
+<dt><a href="/">Christopher Schmidt</a></dt> <dd>Web Developer, Neogeographer</dd>
+</dl>
+<h2>Summary</h2>
+<p>Ever since Google first 'solved' interactive web-based mapping for the masses, providing an API to put your data on top of, people have explored the space at an
+   ever increasing rate. From Google Maps Mashups to all new datasets created out of the existing data, and hacked into that ever-lovable interactive mapping interface, 
+   people are creating new, exciting, and sometimes even innovative ways of looking at existing data.</p>
+ <p> Come discuss the things you've done with maps, the things you're doing with maps, and what you think of the 'mapping revolution'. See what alternatives there are to Google... and why you might not want to be quite so dependant on the corporate data silos.</p>
+
+   
+<h2>Links</h2>
+   <h3>OpenLayers, and its Applications</h3>
+   <ul>
+     <li><a href="http://www.openlayers.org">OpenLayers</a></li>
+     <li><a href="http://boston.freemap.in">Boston Freemap</a></li>
+     <li><a href="http://mumbai.freemap.in">Mumbai Freemap</a></li>
+     <li><a href="http://crschmidt.net/mapping/mit">Mapping MIT</a></li>
+   </ul>
+   <h3>Free Data, US</h3>
+   <ul>
+     <li><a href="http://earth-info.nga.mil/publications/vmap0.html">VMap0</a> -- worldwide, provided by USGS</li>
+     <li><a href="http://geonames.usgs.gov/pls/gnispublic/">Geonames</a> -- worldwide, provided by USGS</li>
+     <li><a href="http://www.census.gov/geo/www/tiger/">TIGER/LINE</a> -- <a href="http://www.census.gov/geo/www/tiger/tiger2005fe/tgr2005fe.html">2005FE Data</a></li>
+   </ul>
+   <h3>Free Data, Mass</h3>
+   <ul>
+     <li><a href="http://www.mass.gov/mgis/">MassGIS</a></li>
+     <li><a href="http://www.mass.gov/mgis/laylist.htm">MassGIS Free Data Layers</a></li>
+   </ul>
+   <h3>Free Data Gathering Projects</h3>
+   <ul>
+     <li><a href="http://www.openstreetmap.org/">OpenStreetMap</a></li>
+     <li><a href="http://boston.openguides.org/">Open Guide to Boston</a> -- part of the <a href="http://openguides.org/">OpenGuides network</a></li>
+   </ul>
+   <h3>Map API Hacking</h3>
+   <ul>
+     <li><a href="http://mapstraction.com/">Mapstraction</a> -- Map API abstraction</li>
+     <li><a href="http://rivermojo.com/">River Mojo</a></li>
+     <li><a href="http://mappinghacks.com/projects/gmaps/cluster.html">Mapping Hacks Clustering</a></li>
+     <li><a href="http://wikimap.csail.mit.edu/">MIT Wikimap</a></li>
+   </ul>
+   <h3>Serverside Rendering Software</h3>
+   <ul>
+     <li><a href="http://mapserver.gis.umn.edu/">UMN Mapserver</a></li>
+     <li><a href="http://docs.codehaus.org/display/GEOS/Home">GeoServer</a></li>
+   </ul>
+   <h3>Employers</h3>
+   <ul>
+     <li><a href="http://www.metacarta.com/">MetaCarta</a> -- Schuyler, Chris</li>
+     <li><a href="http://www.platial.com/">Platial</a> -- Chris</li>
+   </ul>
+<h2>References</h2>
+<ul>
+  <li><a href="http://blog.printf.net/articles/2006/06/03/live-from-barcamp">Chris Ball, Live From Barcamp</a></li>
+</ul>
+<p>If you were participating at any point, and want to add more links to this page, please email <a href="mailto:crschmidt@crschmidt.net">me</a> and let me know.</p>
+</body>
---
+++ present/index.php
@@ -0,0 +1,12 @@
+<?php
+include "../template.php";
+?>
+<title>Presentations</title>
+<body>
+        <p>My  lightning talk called 'Geolocation with GSM Cells' at O'Reilly's <a href="http://conferences.oreillynet.com/where2006/">Where 2.0 conference</a>, on June 13th, 2006 was mentioned in the <a href="http://www.wired.com/news/technology/0,71170-0.html?tw=wn_index_1">Wired Where 2.0 Roundup</a>.</p>
+        <p>I spent a couple hours talking at BarCampBoston about various Geo-related things: Some links from that talk are <a href="/present/barcamp">now available</a>.</p>
+<ul>
+<li><a href="barcamp">BarCampBoston</a></li>
+</ul>
+
+</body>

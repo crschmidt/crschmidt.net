b5cffb7b8ad6b61be623bf8bebdc6bdaa890db08
Christopher Schmidt <crschmidt@crschmidt.net>
2013-11-03 00:56:10 -0400

    Add about directory.
    I have no idea why I have this, but since I do, it can go in the
    "things that exist" bucket.

---
+++ about/logo.php
@@ -0,0 +1,6 @@
+<?  include "../template.php" ?>
+<title>crschmidt.net logo</title>
+<body>
+<p>This page will have data soon.</p>
+<div style="float:right"><object data="/images/logo.svg" width="200px" height="200px" type="image/svg+xml"><embed src="/images/logo.svg" type="image/svg+xml"></object></div>
+</body>

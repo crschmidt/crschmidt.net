563dde4e9473b81fd47d7867b2714d3a71d40726
Christopher Schmidt <crschmidt@crschmidt.net>
2013-11-03 01:32:52 -0400

    A directory originally destined for 'odds and ends', which in the end, only included one thing (which was odd).

---
+++ odds/index.php
@@ -0,0 +1,9 @@
+<? include "../template.php" ?>
+<title>Odds and Ends</title>
+<body>
+<ul>
+<li><a href="wordcompletion.pl">wordcompletion.pl</a> -- irssi script that uses a mysql database to track all words you've ever typed into IRC for the purposes of tab completion.</li>
+<li><a href="wordcompletion.sql.tar.gz">Wordcompletion.sql</a> -- Full SQL dump of /usr/share/dict/american-english into a format compatible with irssi's word completion script, in tarred gzipped format.</a></li>
+
+</ul>
+</body>

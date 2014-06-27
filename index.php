4e95b30ec0a8bfef098a23d0e1012138491e17e2
Christopher Schmidt <crschmidt@crschmidt.net>
2014-06-26 23:25:25 -0400

    Change homepage to have some modern things on it.

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <meta content="text/html; charset=utf-8" http-equiv="content-type" />
     <link rel="meta" title='FOAF' type='application/rdf+xml' href='http://crschmidt.net/foaf.rdf' />
 </headaddl>
-    <title>Christopher Schmidt: GIS and Web Hacker</title>
+    <title>Flying, Photography, and other Geek Pursuits - Christopher Schmidt</title>
 </head>
 <body>
 	    <div id="sidebar">
@@ -55,81 +55,61 @@ if ( $url ) {
 include "links_include.php"
 ?>    </div>
 <p>
-  I am a professional web application developer, and have spent the past several
-  years developing server and client side tools for the creation of web 
-  applications, especially applications which relate to mapping.
-  Some of my most visible work over the past year is in the 
-  OpenLayers/TileCache/FeatureServer stack, a collection of open source 
-  tools designed to help users build mapping applications.
-</p> 
-<h2>Projects</h2>
-<ul style="margin-bottom:10px;">
-<li><a href="http://code.google.com/p/webprocessingserver/">WPServer</a> - RESTful Geodata Processing Server</li>
-<li><a href="http://featureserver.org/">FeatureServer</a> - Online Vector feature storage. Sponsored by <a href="http://www.metacarta.com/">MetaCarta</a>.</li>
-<li><a href="http://tilecache.org/">TileCache</a> - Map tile caching tool. Sponsored by <a href="http://www.metacarta.com/">MetaCarta</a>.</li>
-<li><a href="http://www.openlayers.org/">OpenLayers</a> - Open Soure AJAX Mapping interface. Sponsored by <a href="http://www.metacarta.com/">MetaCarta</a>.</li>
-<li><a href="http://gsmloc.org/">GSMLoc</a> - Cell stumbling web service, Featured on Wired.com</li>
-<li><a href="http://boston.openguides.org/">Open Guide to Boston</a> - part of the <a href="http://openguides.org/">OpenGuides Network</a>.</li> 
-</ul>
-<p>
-  I have experience in a variety of technologies, developed over many years: I
-  have worked on projects ranging from mobile location services to the semantic
-  web, from Python examples to PHP applications on the Ning platform. These
-  days, I work largely with Python on the server, Javascript on the client, and
-  GIS as the application area.
-</p>  
-
-
-<h3>Photography</h3>
-<p>
-  You may have stumbled into me because of seeing some of my photography. I'm
-  an amateur photographer, and most of my photos can be found online via my <a
-  href="http://flickr.com/photos/crschmidt/">Flickr Account</a>. Note that any
-  public photos are released under a CC-By-SA license. This means that you can
-  use them in any way you wish, so long as you allow others to do the same, and
-  give credit to me.
-</p>
-<p>
-  I do ask that if you use my photography, you drop me a note to let me know 
-  when and where you're using it - I do love to see people getting use 
-  out of my work.
-</p>
-
-<h3>Life</h3>
- <p>
-   I'm a married stepfather of two, living in Cambridge, MA. In general, my
-   more personal observations on life take place in my <a
-   href="http://crschmidt.livejournal.com/">LiveJournal</a>. (LiveJournal is an
-   online journalling site -- specifically for me, it's the online journalling
-   site where I met my wife.) 
- </p>
- <p>
-   My <a href="http://flickr.com/photos/crschmidt/">Flickr photos</a> show
-   snapshots of life.
- </p>  
-
-<h3>Older Projects</h3>
-
 <ul>
   <li>
-     <a href="/symbian/">Symbian Hacking</a> - Hacking via Python for my mobile phone.
+    I fly <a href="http://www.flickr.com/photos/nprnncbl/13317084063/">a quadcopter</a>.
   </li>
   <li>
-     <a href="/semweb/">Semantic Web Hacking</a> - Various code and projects I've worked on under the guise of the Semantic Web
+    I <a href="https://www.flickr.com/photos/crschmidt/sets/">take photos</a>. 
   </li>
   <li>
-     <a href="http://sdlroads.sourceforge.net/">SDLRoads</a> - Project written in combination with roommate and a friend, an SDL+OpenGL clone of the DOS game, Skyroads.
+    I <a href="https://www.youtube.com/playlist?list=PLjj2uycFdhzBsNJt0mGqivf960lE9R673">3d print things</a>.
   </li>
   <li>
-     <a href="http://crschmidt.net/dashboard/">Dashboard</a> - Integration of data sources from running applications and outputting useful information taken from clues. Project backend and engine in C#, frontends in whatever language the application supports.
+    I live in Cambridge, Massachusetts.
   </li>
   <li>
-     <a href="http://crschmidt.net/foaf/">FOAF</a> - Friend of a Friend information, patches, etc. Friend of a Friend is an RDF based machine readable format of information about who you are. <a href="http://www.foaf-project.org">FOAF-Project</a> for more.
-  </li>
-  <li>
-     <a href="http://crschmidt.net/povray/">Povray</a> - Ray Tracing program, with links to other competitions plus my own creations.
+    I work for <a href="https://www.youtube.com">YouTube</a>.
   </li>
 </ul>
+
+<h2 style="margin-bottom: 0px;" >Recent Creations</h2>
+<div class="homepageblock">
+<h3>Response to FAA</h3>
+<a href="http://crschmidt.net/blog/archives/696/response-to-new-faa-policy-document/">Response to FAA policy document</a>, a blog post detailing my response to their stated new policies around UAV prosecution.
+<blockquote>This entire policy is based on a mistaken notion that the “the FAA has considered model aircraft to be aircraft that fall within the statutory and regulatory definitions of an aircraft”; the definition that the FAA uses ...  is so broad as to describe everything from a commercial jetliner to a paper airplane</blockquote>
+<small>June 25th, 2014</small>
+</div>
+
+<div class="homepageblock">
+<h3>Quechee Gorge</h3>
+<iframe width="350" height="240" src="//www.youtube.com/embed/WSKBm5m5css?list=UUfoS_RSsblA7N0szMu3btaw" frameborder="0" allowfullscreen></iframe><br />
+Quadcopter flight over Quechee Gorge, include the river and the dam.
+<small>June 23rd, 2014</small>
+</div>
+
+
+<div class="homepageblock">
+<h3>Memorial Day Barbecue</h3>
+<iframe src="https://www.flickr.com/photos/crschmidt/14096095787/in/set-72157644459782639/player/" width="320" height="213" frameborder="0" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe><br />
+Gathering with friends and food over Memorial Day.
+<small>May 26th, 2014</small>
+</div>
+
+<div class="homepageblock">
+<h3>One Minute Onboard</h3>
+<iframe width="350" height="240" src="//www.youtube.com/embed/4zepIltKyE8?list=PLjj2uycFdhzC08S-cUKoOQpfpirFNc6LM" frameborder="0" allowfullscreen></iframe>
+One minute clips of flying the quadcopter (mostly) around Cambridge, MA including flying over the MIT dome, along the Charles River, and at the Arnold Arboretum. Unedited shots, showing the raw footage you get from the quadcopter. 
+<small>May 15th-22nd, 2014</small>
+</div>
+
+<div class="homepageblock">
+<h3>3D Printer Assembly</h3>
+<iframe width="350" height="240" src="//www.youtube.com/embed/C7XUY3wFb74" frameborder="0" allowfullscreen></iframe>
+Time lapse of assembley of Printrbot 3D printer.
+<small>Jan 11, 2014</small>
+</div>
+
 <br clear="both" />
 </body>
 </html>
